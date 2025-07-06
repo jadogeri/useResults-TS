e56b6ddee59956c07fc341e039bf2702a417e667
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-06 17:54:50 -0500

    updated test file test.test.ts

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,7 +1,5 @@
 test('adds 1 + 2 to equal 3', () => {
-    const sum = (n1 : number, n2 : number)=>{
-        return n1 + n2;
-    }
-  expect(sum(1, 2)).toBe(3);
+    const result = 3
+  expect(result).toBe(3);
 });
 
