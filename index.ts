da480127d4c35e2431f616ee610d2301f772bad5
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-07 18:26:42 -0500

    Updated jest.config.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-//import { getParamsAsString } from "./src/getParamsAsString";
-
-//export { getParamsAsString }
 
 import useResults from "./src/useResults";
 
--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
-export const config = {
-    //preset: "ts-jest",
+module.exports = {
+    preset: "ts-jest",
     //testEnvironment: "node",
    // setupFiles: ['./tests/setupTests.ts'],
-    // setupFilesAfterEnv: ['./tests/setupTests.ts'],
+    setupFilesAfterEnv: ['./tests/setupTests.ts'],
       transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest', // Transform .ts and .tsx files with ts-jest
     // Add other transformations if needed, e.g., for CSS or SVG files
